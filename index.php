82e7403d683e7a2d55b7d47b019406d962ad20be
vukojevicc <aleksandar.vukojevicc@gmail.com>
2020-12-26 21:10:14 +0100

    icon added to the title bar and css file was renamed

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="imgs/logo.png">
 </head>
 <body>
     hello world!
