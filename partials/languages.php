697478a3f404ff5398241aa29fbeba0e66377d66
aleksandar <aleksandar.vukojevicc@gmail.com>
2021-01-30 00:47:01 +0100

    glass morphism done for cards section

--- partials/languages.php
+++ partials/languages.php
@@ -1,5 +1,9 @@
 <div class="container">
-    <div class="card"><h2>front end</h2></div>
+    <div class="card">
+    <h2>front&nbsp;end</h2>
+    <img src="imgs/html.png" alt="html">
+    <img src="imgs/css.png" alt="css">
+    </div>
     <div class="card"><h2>server</h2></div>
     <div class="card"><h2>database</h2></div>
     <div class="card"><h2>cms <br> & <br> version control system</h2></div>
