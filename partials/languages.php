eae4103ee3f8baf4a6f9981c8e746c37bb8ff872
aleksandar <aleksandar.vukojevicc@gmail.com>
2021-02-01 15:49:43 +0100

    fade class added to technologies cards

--- partials/languages.php
+++ partials/languages.php
@@ -1,5 +1,5 @@
 <div class="container">
-    <div class="card card1 vertical-alignment-parent">
+    <div class="card card1 vertical-alignment-parent fade">
         <h2>front&nbsp;end</h2>
         <div class="vertical-alignment">
             <div class="card1-grid">
@@ -10,20 +10,20 @@
             </div>
         </div>
     </div>
-    <div class="card card2 vertical-alignment-parent">
+    <div class="card card2 vertical-alignment-parent fade">
         <h2>server</h2>
         <div class="vertical-alignment">
             <img src="imgs/php.png" alt="php">
         </div>
     </div>
-    <div class="card card3 vertical-alignment-parent">
+    <div class="card card3 vertical-alignment-parent fade">
         <h2>cms <br> & <br> version control system</h2>
         <div class="vertical-alignment card3-flex">
             <img class="wp" src="imgs/wordpress.png" alt="wordpress">
             <img class="git" src="imgs/git.png" alt="git">
         </div>
     </div>
-    <div class="card card2 vertical-alignment-parent">
+    <div class="card card2 vertical-alignment-parent fade">
         <h2>database</h2>
         <div class="vertical-alignment">
             <img src="imgs/mysql.png" alt="mysql">
